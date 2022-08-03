39d8cda03eacfa1f3d750d78fc8be997497fac6a
AnhNT257 <nguyenduytuananh98@gmail.com>
2022-08-04 00:55:01 +0700

    first commit

---
+++ app/src/main/java/com/example/baseproject/view/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.baseproject.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.Layout;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.example.baseproject.R;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        private Layout splash;
+//        splash = findViewById(R.id.music_logo);
+//        Animation mAnimation = AnimationUtils.loadAnimation(this, R.anim.logo_animation);
+//        splash.setAnimation(mAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+            }
+        },2000);
+    }
+}
